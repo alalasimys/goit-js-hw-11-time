3aa726ea7aee5036708192d07ebf42f3c0539a09
Alla Karhopolova <alalasimys@gmail.com>
2021-04-05 21:56:01 +0300

    begginng of hm

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  minutes: document.querySelector('span[data-value="mins"]'),
+  seconds: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor() {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalId = null;
+    this.isActive = false;
+    //this.onTick = onTick;
+
+    //this.init();
+  }
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+/*
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2019"),
+}); 
+*/
