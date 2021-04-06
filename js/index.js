df4eb3a729e9b23c1736040819c24f96d8ba10c0
Alla Karhopolova <alalasimys@gmail.com>
2021-04-06 15:16:58 +0300

    finished timer

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
-const refs = {
-  days: document.querySelector('span[data-value="days"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  minutes: document.querySelector('span[data-value="mins"]'),
-  seconds: document.querySelector('span[data-value="secs"]'),
-};
+// const refs = {
+//   days: document.querySelector('span[data-value="days"]'),
+//   hours: document.querySelector('span[data-value="hours"]'),
+//   minutes: document.querySelector('span[data-value="mins"]'),
+//   seconds: document.querySelector('span[data-value="secs"]'),
+// };
 
 class CountdownTimer {
-  constructor() {
-    this.selector = selector;
+  constructor({ selector, targetDate }) {
+    this.element = document.querySelector(selector);
     this.targetDate = targetDate;
-    this.intervalId = null;
-    this.isActive = false;
-    //this.onTick = onTick;
 
-    //this.init();
+    this.initTimer();
   }
+
+  initTimer() {
+    const currentDate = Date.now();
+    let time = this.targetDate.getTime() - currentDate;
+    const intervalId = setInterval(() => {
+      time -= 1000;
+      if (time < 0) {
+        time = 0;
+        clearInterval(intervalId);
+      }
+      const timeComponentsValues = this.getTimeComponents(time);
+      this.render(timeComponentsValues);
+    }, 1000);
+  }
+
   getTimeComponents(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
@@ -22,18 +34,39 @@ class CountdownTimer {
     );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
     return { days, hours, mins, secs };
   }
 
   pad(value) {
     return String(value).padStart(2, "0");
   }
+
+  render({ days, hours, mins, secs }) {
+    return (this.element.innerHTML = `
+    <div class="field">
+      <span class="value" data-value="days">${days}</span>
+      <span class="label">Days</span>
+    </div>
+  
+    <div class="field">
+      <span class="value" data-value="hours">${hours}</span>
+      <span class="label">Hours</span>
+    </div>
+  
+    <div class="field">
+      <span class="value" data-value="mins">${mins}</span>
+      <span class="label">Minutes</span>
+    </div>
+  
+    <div class="field">
+      <span class="value" data-value="secs">${secs}</span>
+      <span class="label">Seconds</span>
+    </div>
+    `);
+  }
 }
 
-/*
-new CountdownTimer({
+const timer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
-}); 
-*/
+  targetDate: new Date("May 2, 2021"),
+});
